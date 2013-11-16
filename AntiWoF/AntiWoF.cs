8c1481091cef1b342800d36b71ac34e580cc4b41
Simon311 <mkontent@gmail.com>
2013-11-16 21:36:00 +0400

    TShock 4.2
    Not sure if works, but should

--- AntiWoF/AntiWoF.cs
+++ AntiWoF/AntiWoF.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using Hooks;
+using TerrariaApi.Server;
 using Terraria;
 
 namespace AntiWoF
 {
-	[APIVersion(1, 12)]
+	[ApiVersion(1, 12)]
 	public class AntiWoF : TerrariaPlugin
 	{
 		public override string Author
@@ -32,19 +32,17 @@ namespace AntiWoF
 		public AntiWoF(Main game)
 			: base(game)
 		{
-			Order = -10;
+			Order = 10;
 		}
 
 		protected override void Dispose(bool disposing)
 		{
-			if (disposing)
-			{
-				NetHooks.GetData -= OnGetData;
-			}
+			if (disposing) ServerApi.Hooks.NetGetData.Register(this, OnGetData, 10);
+			base.Dispose(disposing);
 		}
 		public override void Initialize()
 		{
-			NetHooks.GetData += OnGetData;
+			ServerApi.Hooks.NetGetData.Deregister(this, OnGetData);
 		}
 
 		void OnGetData(GetDataEventArgs e)
